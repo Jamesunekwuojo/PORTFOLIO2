1b5bcf22812bfcaecbbb561246281e129271a587
jamesunekwuojo <unekwuojoabah4@gmail.com>
2023-11-17 21:01:30 +0100

    animated circles removed

--- portfolio.js
+++ portfolio.js
@@ -1,14 +1,14 @@
 const textElement = document.getElementById('Bodytext');
-const textToType = "Hi welcome to James Unekwuojo page. A fullstack developer in making. "; // Text to be typed
+const textToType = "Hi, welcome to James Unekwuojo page. A fullstack developer in making. "; // Text to be typed
 let index = 0;
 
 function typeWriter() {
   if (index < textToType.length) {
     textElement.innerHTML += textToType.charAt(index);
     index++;
-    setTimeout(typeWriter, 100); // Change typing speed here (milliseconds)
+    setTimeout(typeWriter, 100); 
   }
 }
 
-// Start typing animation
+
 typeWriter();
