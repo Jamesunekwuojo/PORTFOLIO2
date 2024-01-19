cc495704ba7bed07a3d5ddf09a7f81060382d588
jamesunekwuojo <unekwuojoabah4@gmail.com>
2024-01-20 00:15:51 +0100

    light/sark mode toggle removed

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
       searchBtn = body.querySelector(".search-box"),
-      modeSwitch = body.querySelector(".toggle-switch"),
+      modeSwitch = body.querySelector(".sun-moon "),
       modeText = body.querySelector(".mode-text");
 
 
@@ -18,9 +18,9 @@ modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
     
     if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
         modeText.innerText = "Dark mode";
+    }else{
+        modeText.innerText = "Light-mode";
         
     }
 });
